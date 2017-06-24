3607bde56ea7645913fb17c9d731fab8e542d01f
Marc Altmann <marcaltmann@posteo.de>
2017-06-24 09:38:09 +0200

    Implement search

--- db/structure.sql
+++ db/structure.sql
@@ -175,6 +175,27 @@ CREATE UNIQUE INDEX index_customers_on_email ON customers USING btree (email);
 
 
 --
+-- Name: index_customers_on_lower_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_customers_on_lower_email ON customers USING btree (lower((email)::text));
+
+
+--
+-- Name: index_customers_on_lower_first_name_varchar_pattern_ops; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_customers_on_lower_first_name_varchar_pattern_ops ON customers USING btree (lower((first_name)::text) varchar_pattern_ops);
+
+
+--
+-- Name: index_customers_on_lower_last_name_varchar_pattern_ops; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_customers_on_lower_last_name_varchar_pattern_ops ON customers USING btree (lower((last_name)::text) varchar_pattern_ops);
+
+
+--
 -- Name: index_customers_on_username; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -204,6 +225,7 @@ SET search_path TO "$user",public;
 INSERT INTO "schema_migrations" (version) VALUES
 ('20170623175227'),
 ('20170623185600'),
-('20170623204644');
+('20170623204644'),
+('20170624072752');
 
 
